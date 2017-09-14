b15d010caaf78a8ae04d5e51f93a91f6e40916b4
freelattice <wannachaos@gmail.com>
2017-09-13 20:43:38 -0700

    Create removingpebblegame.py

---
+++ main/removingpebblegame.py
@@ -0,0 +1,32 @@
+import sys
+
+
+def f(n, m, M):
+    if M[n][m] is not None:
+        return M[n][m]
+    if m <= 2**(n - 1):
+        M[n][m] = bool(n % 2)
+        return bool(n % 2)
+    if n % 2:
+        for x in range(1, 2**(n - 1) + 1):
+            if f(n+1, m-x, M):
+                M[n][m] = True
+                return True
+        M[n][m] = False
+        return False
+    else:
+        for x in range(1, 2**(n - 1) + 1):
+            if not f(n + 1, m - x, M):
+                M[n][m] = False
+                return False
+        M[n][m] = True
+        return True
+
+
+def ff(m):
+    M = [[None] * (m+1) for i in range(len(bin(m)))]
+    return ('Onii-chan' if f(1, m, M) else 'Imouto') + ' has a winning strategy.'
+
+
+if __name__ == "__main__":
+    print(ff(int(sys.argv[1])))
